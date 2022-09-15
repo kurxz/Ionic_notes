bd2e61d101b6e62c19e7b65a2da54b6b1be0ff31
KurXZ <29847942+kurxz@users.noreply.github.com>
2022-09-15 19:48:41 -0300

    Created Filter Pipe to handle search of home page

---
+++ src/app/Pipes/filter.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "filter",
+})
+export class FilterPipe implements PipeTransform {
+  transform(data: any, args: any): unknown {
+    if (!data || !args) {
+      return data;
+    }
+
+    var filteredData = data.filter((element) => {
+      if (element.list.text != null && element.list.text.includes(args)) {
+        return true;
+      }
+
+      if (element.list.myLists != null) {
+        return element.list.myLists.some((data) => {
+          if (data.item.includes(args)) {
+            return true;
+          }
+        });
+      }
+    });
+
+    return filteredData;
+  }
+}
